bf4dc99db0d50280e05f9d69940890584e9a2b7c
andrea cadeddu <cadedduan@gmail.com>
2018-03-26 14:19:29 -0500

    add epicycle to support proxy+/proxy

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 
+
 module.exports.customDomainReroute = (req,res,next) => {
     if (!! req.headers['x-apigateway-event']){
         const event = JSON.parse(decodeURIComponent(req.headers['x-apigateway-event']));
         const params = event.pathParameters || {};
    
+
+        const replace_params = (acc, k) => {
+            if (k =='proxy'){
+                return acc.replace('{proxy+}', params[k])
+            }
+            else {
+                return acc.replace('{'+k+'}', params[k])
+            }
+        }
+
+
         let interpolated_resource = Object.keys(params)
-            .reduce((acc, k) => acc.replace('{'+k+'}', params[k]), event.resource)
+            .reduce(replace_params, event.resource)
         
         if ((!! event.path && !! interpolated_resource) && event.path != interpolated_resource){
             req.url = req.originalUrl = interpolated_resource;
