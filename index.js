30e1692d8d6731060beb04a6b147935341dce3c0
Andy Stocchetti <ajstocchetti@gmail.com>
2018-09-25 10:21:23 -0500

    create option for custom logger

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-module.exports.customDomainReroute = (req, res, next) => {
-  if (!!req.headers['x-apigateway-event']) {
-    const event = JSON.parse(decodeURIComponent(req.headers['x-apigateway-event']));
-    const params = event.pathParameters || {};
+module.exports = {
+  customDomainReroute: setup({ logger: console.log });
+  setup,
+};
 
-    const replace_params = (acc, k) => {
-      if (k == 'proxy') {
-        return acc.replace('{proxy+}', params[k])
-      } else {
-        return acc.replace('{' + k + '}', params[k])
+function setup(opts = {}) {
+  return function routerMiddleware(req, res, next) {
+    if (!!req.headers['x-apigateway-event']) {
+      const event = JSON.parse(decodeURIComponent(req.headers['x-apigateway-event']));
+      const params = event.pathParameters || {};
+
+      const replace_params = (acc, k) => {
+        if (k == 'proxy') {
+          return acc.replace('{proxy+}', params[k])
+        } else {
+          return acc.replace('{' + k + '}', params[k])
+        }
+      }
+
+      let interpolated_resource = Object.keys(params)
+        .reduce(replace_params, event.resource);
+
+      //covers trailing slash cornercase, since trailing slashes are not returned in event.resource .
+      if (event.path.endsWith('/') && !interpolated_resource.endsWith('/')) {
+        interpolated_resource = `${interpolated_resource}/`;
+      }
+
+      if ((!!event.path && !!interpolated_resource) && event.path != interpolated_resource) {
+        req.url = req.originalUrl = interpolated_resource;
+        if (opts.logger && typeof opts.logger === 'function') {
+          opts.logger(`rerouted ${event.path} -> ${interpolated_resource}`);
+        }
       }
     }
-
-    let interpolated_resource = Object.keys(params)
-      .reduce(replace_params, event.resource);
-
-    //covers trailing slash cornercase, since trailing slashes are not returned in event.resource .
-    if (event.path.endsWith('/') && !interpolated_resource.endsWith('/')) {
-      interpolated_resource = `${interpolated_resource}/`;
-    }
-
-    if ((!!event.path && !!interpolated_resource) && event.path != interpolated_resource) {
-      req.url = req.originalUrl = interpolated_resource;
-      console.log(`rerouted ${event.path} -> ${interpolated_resource}`);
-    }
+    next();
   }
-  next();
 }
