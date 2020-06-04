8992a7f8c67a6ada9754e3db6d9e562e0ca59fea
Paul Roberts <psxpaul@gmail.com>
2020-06-04 14:38:35 -0700

    Add type declaration for setup function

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,5 @@ declare module '@turinggroup/serverless-express-custom-domain-middleware' {
     import { NextHandleFunction } from 'connect';
 
     export const customDomainReroute: NextHandleFunction;
+    export const setup: (() => NextHandleFunction);
 }
